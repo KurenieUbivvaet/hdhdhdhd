d18410dc65c2e48cb1e5b7459e57feac8e9d326e
Mikhail <studentipmadk@gmail.com>
2024-04-24 17:58:36 +0300

    11

---
+++ Technoservice/EnterForm.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Technoservice
+{
+    partial class EnterForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            LoginBox = new TextBox();
+            PasswordBox = new TextBox();
+            RegBtn = new Button();
+            PasHideBtn = new Button();
+            EnterBtn = new Button();
+            SuspendLayout();
+            // 
+            // LoginBox
+            // 
+            LoginBox.Location = new Point(77, 34);
+            LoginBox.Name = "LoginBox";
+            LoginBox.Size = new Size(100, 23);
+            LoginBox.TabIndex = 0;
+            // 
+            // PasswordBox
+            // 
+            PasswordBox.Location = new Point(77, 72);
+            PasswordBox.Name = "PasswordBox";
+            PasswordBox.Size = new Size(100, 23);
+            PasswordBox.TabIndex = 1;
+            // 
+            // RegBtn
+            // 
+            RegBtn.Location = new Point(77, 135);
+            RegBtn.Name = "RegBtn";
+            RegBtn.Size = new Size(100, 23);
+            RegBtn.TabIndex = 2;
+            RegBtn.Text = "Регистрация";
+            RegBtn.UseVisualStyleBackColor = true;
+            RegBtn.Click += RegBtn_Click;
+            // 
+            // PasHideBtn
+            // 
+            PasHideBtn.Location = new Point(183, 71);
+            PasHideBtn.Name = "PasHideBtn";
+            PasHideBtn.Size = new Size(24, 23);
+            PasHideBtn.TabIndex = 3;
+            PasHideBtn.Text = "X";
+            PasHideBtn.UseVisualStyleBackColor = true;
+            PasHideBtn.Click += PasHideBtn_Click;
+            // 
+            // EnterBtn
+            // 
+            EnterBtn.Location = new Point(77, 106);
+            EnterBtn.Name = "EnterBtn";
+            EnterBtn.Size = new Size(100, 23);
+            EnterBtn.TabIndex = 4;
+            EnterBtn.Text = "Вход";
+            EnterBtn.UseVisualStyleBackColor = true;
+            EnterBtn.Click += EnterBtn_Click;
+            // 
+            // EnterForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(592, 275);
+            Controls.Add(EnterBtn);
+            Controls.Add(PasHideBtn);
+            Controls.Add(RegBtn);
+            Controls.Add(PasswordBox);
+            Controls.Add(LoginBox);
+            Name = "EnterForm";
+            Text = "EnterForm";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox LoginBox;
+        private TextBox PasswordBox;
+        private Button RegBtn;
+        private Button PasHideBtn;
+        private Button EnterBtn;
+    }
+}
---
+++ Technoservice/EnterForm.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.VisualBasic.Logging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Technoservice.Collection;
+using Technoservice.Models;
+
+namespace Technoservice
+{
+    public partial class EnterForm : Form
+    {
+        const string name = "Залупка";
+        const string surname = "Залупкин";
+        const int RoleId = 1;
+        private Form1 form1 = new Form1();
+        public EnterForm()
+        {
+            InitializeComponent();
+        }
+
+        private void RegBtn_Click(object sender, EventArgs e)
+        {
+            if (LoginBox.Text.Length == 0 || PasswordBox.Text.Length == 0)
+            {
+                MessageBox.Show("Вы не заполнили!");
+            }
+            else
+            {
+                using (TehnoserviceContext db = new TehnoserviceContext())
+                {
+                    string login = LoginBox.Text;
+                    var LogPas = db.Users.Where(x => x.Login == login);
+                    if (LogPas.Any())
+                    {
+                        MessageBox.Show("Занято");
+                    }
+                    else
+                    {
+                        Users users = new Users()
+                        {
+                            UserName = name,
+                            UserSurname = surname,
+                            Login = LoginBox.Text,
+                            Password = md5.hashPassword(PasswordBox.Text),
+                            IdRole = RoleId
+                        };
+                        db.Users.Add(users);
+                        db.SaveChanges();
+                        MessageBox.Show("збс");
+                    }
+                }
+            }
+        }
+
+        private void PasHideBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EnterBtn_Click(object sender, EventArgs e)
+        {
+            if (LoginBox.Text.Length == 0 || PasswordBox.Text.Length == 0)
+            {
+                MessageBox.Show("Пустые поля");
+            }
+            else
+            {
+                string login = LoginBox.Text;
+                string password = md5.hashPassword(PasswordBox.Text);
+                using(TehnoserviceContext ctx = new TehnoserviceContext())
+                {
+                    var LogPas = ctx.Users.Where(x => x.Login == login && x.Password == password);
+                    foreach (var user in LogPas)
+                    {
+                        if (user.IdRole != 1)
+                        {
+                            MessageBox.Show("Роль говно");
+                        }
+                        else
+                        {
+                            form1.label1.Text = user.Login;
+                            form1.Show();
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
